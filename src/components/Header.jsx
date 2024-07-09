e10692f61b8a5db15af16504a35318f5ae96b468
Badmatrix <matrix.og11@gmail.com>
2024-07-09 23:07:26 +0100

    image update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header({carts}) {
   return (
     <header className="relative w-full bg-wine-400 h-[300px]">
       <img
-        src="/public/images/deleece-cook-1w8GFi871tg-unsplash.jpg"
+        src="/images/Frame 152 (1).svg"
         alt="nav-image"
         className="h-full w-full object-cover"
       />
